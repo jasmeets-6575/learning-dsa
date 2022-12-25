623f58a780303ae5dbc27934c6eb998f34bc80a9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-25 22:15:43 +0530

    bubble sort

---
+++ By Codevolution/algorithms/bubbleSort.js
@@ -0,0 +1,20 @@
+function sorting(arr) {
+    let swapped
+    do {
+        swapped = false
+        for (let i = 0; i < arr.length - 1; i++) 
+        {
+            if (arr[i] > arr[i + 1]) 
+            {
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp;
+                swapped = true
+            }
+        }
+    } 
+    while(swapped) 
+}
+const arr = [6, -5, -2, 3, 10, 1, 15]
+sorting(arr)
+console.log(arr);
