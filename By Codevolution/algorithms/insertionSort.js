a351a21197e06a9d8c616b240d65f16d13736ab5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-27 23:12:07 +0530

    quick sort

--- By Codevolution/algorithms/insertionSort.js
+++ By Codevolution/algorithms/insertionSort.js
@@ -1,12 +1,12 @@
 function insertionSort(arr) {
-    for (let i = 1; i < arr.length ; i++){
+    for (let i = 1; i < arr.length; i++) {
         let numberToInsert = arr[i]
         let j = i - 1;
-        while (j >= 0 && arr[j] > numberToInsert){
-            arr[j+1] = arr[j]
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j]
             j = j - 1
-        } 
-        arr[j+1] = numberToInsert
+        }
+        arr[j + 1] = numberToInsert
     }
 }
 
