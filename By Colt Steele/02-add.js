55147b60b99a7dbfada5566aebb65c26f325e9cc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-18 03:29:53 +0530

    time complexity

---
+++ By Colt Steele/02-add.js
@@ -0,0 +1,22 @@
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// let t1 = performance.now();
+// addUpTo(1000000000);
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+console.log(addUpTo(100));
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
