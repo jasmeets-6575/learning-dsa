1c8a36012fcb022c0a6be3a6f22aa05e31729d60
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-20 04:55:40 +0530

    refactoring char count code

--- By Colt Steele/02-problem-solving-approach/01-count-char.js
+++ By Colt Steele/02-problem-solving-approach/01-count-char.js
@@ -1,21 +1,39 @@
+// function charCount(str) {
+//   // make object to return at end
+//   var result = {};
+//   // loop over string, for each character...
+//   for (var i = 0; i < str.length; i++) {
+//     // if the char is a number/ letter AND is a key in object, add one to count
+//     var char = str[i].toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       if (result[char] > 0) {
+//         result[char]++;
+//       }
+//       // if the char is a number/ letter AND not in object , add it to object and set value to 1
+//       else {
+//         result[char] = 1;
+//       }
+//     }
+//   }
+
+//   // if character is something else (space, period, etc.) don't do anything
+//   // return object at end
+//   return result;
+// }
+
 function charCount(str) {
-  // make object to return at end
   var result = {};
-  // loop over string, for each character...
-  for (var i = 0; i < str.length; i++) {
-    // if the char is a number/ letter AND is a key in object, add one to count
-    var char = str[i].toLowerCase() 
-    if (result[char] > 0) {
-      result[char]++;
-    }
-    // if the char is a number/ letter AND not in object , add it to object and set value to 1
-    else {
-      result[char] = 1;
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0) {
+        result[char]++;
+      }
+      else {
+        result[char] = 1;
+      }
     }
   }
-
-  // if character is something else (space, period, etc.) don't do anything
-  // return object at end
   return result;
 }
 
