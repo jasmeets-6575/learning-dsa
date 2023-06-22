43a9e87c3c3537c95e56a98f5c11b46e6b779cbb
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-22 08:01:51 +0530

    counter unique values test

---
+++ by-colt-steele/02-problem-solving-approach/05-count-Unique-values.js
@@ -0,0 +1,16 @@
+function countUniqueValues(arr) {
+  let values;
+  if (arr.length < 1) {
+    values = 0;
+  } else {
+    values = 1;
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      values = (values || 0) + 1;
+    }
+  }
+  return values;
+}
+
+console.log(countUniqueValues([-1, 1]));
