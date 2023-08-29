ed51984ac8814a7d55452a40f95873cbe845a2e8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-29 11:38:28 +0530

    productOfArray

--- by-colt-steele/04-recursion/08-recursions-examples.js
+++ by-colt-steele/04-recursion/08-recursions-examples.js
@@ -1,5 +1,12 @@
-function factorial(num) {
-  if (num === 0) return 1;
-  return num * factorial(--num);
+// function factorial(num) {
+//   if (num === 0) return 1;
+//   return num * factorial(--num);
+// }
+// console.log(factorial(5));
+
+function productOfArray(arr) {
+  if (arr.length === 1) return arr[0];
+  return arr[0] * productOfArray(arr.splice(1));
 }
-console.log(factorial(5));
+
+console.log(productOfArray([3]));
