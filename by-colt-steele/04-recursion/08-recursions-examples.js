8c7627804e619a0c0658d63bfc92498a29a32e51
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-29 11:43:30 +0530

    recursiveRange

--- by-colt-steele/04-recursion/08-recursions-examples.js
+++ by-colt-steele/04-recursion/08-recursions-examples.js
@@ -4,9 +4,16 @@
 // }
 // console.log(factorial(5));
 
-function productOfArray(arr) {
-  if (arr.length === 1) return arr[0];
-  return arr[0] * productOfArray(arr.splice(1));
+// function productOfArray(arr) {
+//   if (arr.length === 1) return arr[0];
+//   return arr[0] * productOfArray(arr.splice(1));
+// }
+
+// console.log(productOfArray([3]));
+
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(--num);
 }
 
-console.log(productOfArray([3]));
+console.log(recursiveRange(6));
