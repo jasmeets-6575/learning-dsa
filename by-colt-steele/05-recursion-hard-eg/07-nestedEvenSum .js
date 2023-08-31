786cdbf17bdcc5e0f0d366547a62acfbf47c74a6
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-31 20:04:21 +0530

    nestedEvenSum

---
+++ by-colt-steele/05-recursion-hard-eg/07-nestedEvenSum .js
@@ -0,0 +1,23 @@
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+};
+
+function nestedEvenSum(obj, sum = 0) {
+  for (var key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]);
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
+console.log(nestedEvenSum(obj1));
