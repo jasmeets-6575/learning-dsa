7f0d79cc88287922c35526d6d13b084ed460acaf
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-10 16:02:21 +0530

    swap method new

--- by-colt-steele/07-bubble-sort/04-bubbleSort.js
+++ by-colt-steele/07-bubble-sort/04-bubbleSort.js
@@ -1,10 +1,23 @@
+// function bubbleSort(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         var temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr.length; j++) {
       if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        swap(arr, j, j + 1);
       }
     }
   }
