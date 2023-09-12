faeaaa6aa0655f086ab872db449562a912e59d19
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-12 07:38:03 +0530

    es 2015 selectionSort

--- by-colt-steele/07-sorting/06-selectionSort.js
+++ by-colt-steele/07-sorting/06-selectionSort.js
@@ -1,4 +1,23 @@
+// function selectionSort(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     var lowest = i;
+//     for (var j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[lowest]) {
+//         lowest = j;
+//       }
+//     }
+//     if (i !== lowest) {
+//       var temp = arr[i];
+//       arr[i] = arr[lowest];
+//       arr[lowest] = temp;
+//     }
+//   }
+//   return arr;
+// }
 function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
   for (var i = 0; i < arr.length; i++) {
     var lowest = i;
     for (var j = i + 1; j < arr.length; j++) {
@@ -6,11 +25,7 @@ function selectionSort(arr) {
         lowest = j;
       }
     }
-    if (i !== lowest) {
-      var temp = arr[i];
-      arr[i] = arr[lowest];
-      arr[lowest] = temp;
-    }
+    if (i !== lowest) swap(arr, i, lowest);
   }
   return arr;
 }
