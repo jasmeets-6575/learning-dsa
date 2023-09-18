fa31e188333dd27b39f7fc5a20477b5b2faebcca
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-18 17:03:27 +0530

    condition check

--- by-colt-steele/07-sorting/08-mergeSort.js
+++ by-colt-steele/07-sorting/08-mergeSort.js
@@ -3,21 +3,24 @@ function mergeSort(arr1, arr2) {
   let i = 0;
   let j = 0;
 
-  while (i < arr1.length && arr2.length) {
+  while (i < arr1.length && j < arr2.length) {
     if (arr2[j] > arr1[i]) {
       results.push(arr1[i]);
       i++;
     } else {
-      results.push(arr1[j]);
+      results.push(arr2[j]);
       j++;
     }
   }
   while (i < arr1.length) {
     results.push(arr1[i]);
+    i++;
   }
   while (j < arr2.length) {
-    results.push(arr1[j]);
+    results.push(arr2[j]);
+    j++;
   }
+  return results;
 }
 
 console.log(mergeSort([1, 10, 50], [2, 14, 99, 100]));
