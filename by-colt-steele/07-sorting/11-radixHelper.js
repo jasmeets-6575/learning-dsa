b2e5340a31f83993af08aabc0cd7c0880e545c1a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-25 20:21:43 +0530

    radix helpers

--- by-colt-steele/07-sorting/11-radixHelper.js
+++ by-colt-steele/07-sorting/11-radixHelper.js
@@ -3,3 +3,10 @@ function getDigit(num, i) {
 }
 
 console.log(getDigit(12345, 2));
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+console.log(digitCount(1212144));
