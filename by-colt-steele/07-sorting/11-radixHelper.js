ac1acf386f3b527c8f00443fa78bc91f8f0f8b5b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-01 10:23:06 +0530

    mostDigits

--- by-colt-steele/07-sorting/11-radixHelper.js
+++ by-colt-steele/07-sorting/11-radixHelper.js
@@ -2,11 +2,15 @@ function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
-console.log(getDigit(12345, 2));
-
 function digitCount(num) {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-console.log(digitCount(1212144));
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
